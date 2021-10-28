d227cd88e2ce00e61f9dea25d138df750647aad4
Hector <hectorm.almaguerc@gmail.com>
2021-10-28 12:43:54 -0500

    Cambios

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,6 @@
 function validarFormulario() {
     var email = document.getElementById('email');
     if (!(/\S+@\S+\.\S+/.test(email))){
-        alert('ERROR: Debe escribir un correo válido');
         return false;
     }
 }
