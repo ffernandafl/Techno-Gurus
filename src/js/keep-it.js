84e446510bca241331708534c08f46c0fa6527c0
Diana Marlene <diana.sanchez9907@gmail.com>
2021-11-03 13:52:03 -0600

    cambios kiss

--- src/js/keep-it.js
+++ src/js/keep-it.js
@@ -2,7 +2,9 @@ var next = document.getElementById("btn-next-kiss");
 next.addEventListener("click", nextKiss);
 
 function nextKiss() {
+    document.getElementById("title-idea").innerHTML = "Sobre saturno y su esplendor";
     document.getElementById("idea").innerHTML = "Saturno es el sexto planeta del sistema solar empezando a contar desde nuestra estrella hacia el exterior. Se trata de un planeta gigante gaseoso, igual que Júpiter, Urano y Neptuno. Sin embargo, más allá de lo que constituye el aspecto del propio planeta, en su plano ecuatorial y separados por miles de kilómetros del planeta, encontramos una serie de anillos concéntricos que se mantienen imperturbables alrededor del planeta. Estos anillos se mantienen en esta posición porque están orbitando alrededor de Saturno, del mismo modo que lo haría cualquier satélite natural en torno a un planeta.";
+    
 }
 
 
