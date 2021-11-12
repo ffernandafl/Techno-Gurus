a139bd71c2a82b4f9c53752cb29195be94bf485e
Fernanda Flores <ffernandaflorescarrillo@gmail.com>
2021-11-12 15:32:38 -0600

    sign up

--- src/sql/kiss.sql
+++ src/sql/kiss.sql
@@ -3,35 +3,34 @@ CREATE DATABASE kiss;
 USE kiss;
 
 CREATE TABLE users(
-    id_user      INT AUTO_INCREMENT NOT NULL,
+    id_user      BIGINT AUTO_INCREMENT NOT NULL,
     full_name    VARCHAR(255) NOT NULL,
     email        VARCHAR(255) NOT NULL,
-    entry_key    VARCHAR(50) NOT NULL,
-    cel          VARCHAR(15),
-
+    entry_key    VARCHAR(50) NOT NULL
+    
     CONSTRAINT pk_user PRIMARY KEY (id_user)
 )
 
 CREATE TABLE post(
     id_post         INT AUTO_INCREMENT NOT NULL,
-    id_user         INT NOT NULL,
+    id_user         BIGINT NOT NULL,
     title           VARCHAR(50) NULL,
-    body            NVARCHAR(3720) NULL, 
-    id_post_father  INT NULL,
-    id_post_krush   INT NULL,
+    body            TEXT NULL,
+    id_post_krush   INT NULL
 
     CONSTRAINT pk_post PRIMARY KEY (id_post),
     CONSTRAINT FOREIGN KEY (id_user) REFERENCES users(id_user)
 )
 
-/*CREATE TABLE pagos(
-    id_post         INT AUTO_INCREMENT NOT NULL,
-    id_user         INT NOT NULL,
-    title           VARCHAR(50) NULL,
-    body            NVARCHAR(3720) NULL, 
-    id_post_father  INT NULL,
-    id_post_krush   INT NULL
 
-    CONSTRAINT pk_post PRIMARY KEY (id_post),
-    CONSTRAINT FOREIGN KEY (id_user) REFERENCES users(id_user)
-);*/
+/* CONSULTA */
+
+SELECT  d.id_post AS id_post,
+        d.title AS title,
+        d.body AS body,
+        a.id_post AS id_post_krush,
+        a.title AS title_krush,
+        a.body AS body_krush
+FROM post d
+JOIN post a
+ON d.id_post_krush = a.id_post
